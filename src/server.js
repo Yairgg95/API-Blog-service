aa71be3bd0b3af49375e35c264e7dc66973a989c
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2024-06-10 14:32:08 -0600

    [FEAT]Add server.js logic

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 const cors = require('cors');
 const express = require('express');
+
 // routers
+const authRouter = require('./routes/auth.router');
+const usersRouter = require('./routes/users.router');
+const postsRouter = require('./routes/posts.router');
 
 const app = express();
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+
 // app.use(/rutas, ruta)
+app.use('/auth', authRouter);
+app.use('/user', usersRouter);
+app.use('/posts', postsRouter);
 
 
 app.get('/',(req, res) => {
